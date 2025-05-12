a22e85dca929f0bf254feeaee08e03b89d7921cc
MunSeungJun <sbs2469@naver.com>
2025-05-12 16:26:58 +0900

    feat: update reviewSchema

--- models/review.js
+++ models/review.js
@@ -1,26 +1,34 @@
 import mongoose from "mongoose";
 
 const ReviewSchema = new mongoose.Schema({
-  user: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: "User" 
-},
-  store: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: "Store" 
-},
-  rating: { 
-    type: Number, 
-    min: 1, 
-    max: 5 
-},
-  comment: { 
-    type: String 
-},
-  createdAt: { 
-    type: Date, 
-    default: Date.now 
-},
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  store: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Store",
+  },
+  author: {
+    name: String,
+    profileImage: String,
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+  },
+  comment: {
+    type: String,
+  },
+  isExternal: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 export default ReviewSchema;
