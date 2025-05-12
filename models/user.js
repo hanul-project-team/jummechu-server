211464aea87c2ae79b3e0d95c7ae71674c54a255
MunSeungJun <sbs2469@naver.com>
2025-05-12 17:36:10 +0900

    feat: add profileImage filed

--- models/user.js
+++ models/user.js
@@ -8,11 +8,15 @@ const UserSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    require: true,
+    required: true,
   },
   name: {
     type: String,
     required: true,
+  },
+  profileImage: {
+    type: String,
+    default: "",
   },
   role: {
     type: String,
