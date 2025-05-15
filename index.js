862c08c8a19c7e248d0d3d96166dea53b3b03311
MunSeungJun <sbs2469@naver.com>
2025-05-15 13:01:36 +0900

    fix: cors 미들웨어 적용 우선 순위 가장 앞으로 변경

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ import authRouter from './routes/auth.js'
 
 const app = express()
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true
 }))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 connect()
 app.use('/', indexRouter)
