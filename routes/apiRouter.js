6bc75d77c871c39781ad203c422b9986832a4af6
STMT <elahep113@naver.com>
2025-05-15 18:06:55 +0900

    chore: 카카오 api 위치 기반으로 정보를 받아올 수 있도록 추가

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
 import axios from "axios";
 const router = express.Router();
-import 'dotenv/config'
+import "dotenv/config";
 
 router.post("/search", async (req, res) => {
   const placeName = req.body.place;
+  const location = req.body.location;
   console.log('검색어:',placeName);
+  // console.log('좌표:',location);
   try {
     const document = await axios
       .get("https://dapi.kakao.com/v2/local/search/keyword.json", {
@@ -16,14 +18,21 @@ router.post("/search", async (req, res) => {
           query: placeName,
           page: 2,
           size: 10,
+          category_group_code: "FD6",
+          x: location.lng,
+          y: location.lat,
+          radius: 1500,
         },
       })
-      const kakaoPlace = document.data.documents
-    //   console.log(kakaoPlace)
-    res.status(200).send(kakaoPlace)
+      .catch((err) => {
+        console.log("에러 발생", err);
+      });
+    const kakaoPlace = document.data.documents;
+    // console.log(document.data);
+    res.status(200).send(kakaoPlace);
   } catch (err) {
     // console.log(err);
-    res.status(500).send("카카오 api 호출 실패")
+    res.status(500).send("카카오 api 호출 실패");
   }
 });
 
