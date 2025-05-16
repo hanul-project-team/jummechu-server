bed5d54ca4b3cbc0d97235b90c4d459012766d86
STMT <elahep113@naver.com>
2025-05-16 10:29:53 +0900

    chore: google 루트 요청 주석 google api 사용 보류에 따른 주석처리

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,29 +4,30 @@ const router = express.Router();
 import 'dotenv/config'
 
 router.get("/", (req, res) => {
-  const lat = req.headers.location.slice(1, -1).split(",")[0].trim();
-  const lng = req.headers.location.slice(1, -1).split(",")[1].trim();
-  try {
-    const googlePlaces = axios
-      .get(
-        `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%2C${lng}&radius=500&type=restaurant&key=${process.env.API_KEY}`,
-        {
-          withCredentials: true,
-        }
-      )
-      .then((results) => {
-        res.status(200).json(results.data.results);
-        // console.log(results.data.results)
-        // console.log('응답 완료')
-      })
-      .catch((err) => {
-        res.status(500).json({
-          msg: "place 요청 실패",
-        });
-      });
-  } catch (err) {
-    console.log(err);
-  }
+  res.send('hello World')
+  // const lat = req.headers.location.slice(1, -1).split(",")[0].trim();
+  // const lng = req.headers.location.slice(1, -1).split(",")[1].trim();
+  // try {
+  //   const googlePlaces = axios
+  //     .get(
+  //       `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%2C${lng}&radius=500&type=restaurant&key=${process.env.API_KEY}`,
+  //       {
+  //         withCredentials: true,
+  //       }
+  //     )
+  //     .then((results) => {
+  //       res.status(200).json(results.data.results);
+  //       // console.log(results.data.results)
+  //       // console.log('응답 완료')
+  //     })
+  //     .catch((err) => {
+  //       res.status(500).json({
+  //         msg: "place 요청 실패",
+  //       });
+  //     });
+  // } catch (err) {
+  //   console.log(err);
+  // }
 });
 
 export default router;
