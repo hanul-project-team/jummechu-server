17f6318d22b71a1ee8168896c11acd8fcb8e8608
STMT <elahep113@naver.com>
2025-05-16 18:13:22 +0900

    chore: kakako maps api 사용에 따른 코드 변경 1. search post를 따로 추가 2. 기존 주변 정보는 nearplace로 변경

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -3,33 +3,64 @@ import axios from "axios";
 const router = express.Router();
 import "dotenv/config";
 
-router.post("/search", async (req, res) => {
-  const placeName = req.body.place;
-  const location = req.body.location;
-  console.log('검색어:',placeName);
-  // console.log('좌표:',location);
+router.post("/nearplace", async (req, res) => {
+  const center = req.body.location;
+  // console.log(center);
+  // res.json(center)
   try {
-    const document = await axios
+    const kakaoRespond = await axios
       .get("https://dapi.kakao.com/v2/local/search/keyword.json", {
         headers: {
           Authorization: `KakaoAK ${process.env.KAKAO_KEY}`,
         },
         params: {
-          query: placeName,
+          query: "맛집",
           page: 2,
           size: 10,
           category_group_code: "FD6",
-          x: location.lng,
-          y: location.lat,
+          x: center.lng,
+          y: center.lat,
           radius: 1500,
         },
       })
       .catch((err) => {
         console.log("에러 발생", err);
       });
-    const kakaoPlace = document.data.documents;
-    // console.log(document.data);
-    res.status(200).send(kakaoPlace);
+    const data = kakaoRespond.data.documents;
+    // console.log(data)
+    res.status(200).json(data);
+  } catch (err) {
+    // console.log(err);
+    res.status(500).send("카카오 api 호출 실패");
+  }
+});
+router.post("/search", async (req, res) => {
+  const query = req.body.place;
+  const center = req.body.center;
+  try {
+    console.log("검색어:", query);
+    console.log("좌표", center);
+    //   const kakaoRespond = await axios
+    //     .get("https://dapi.kakao.com/v2/local/search/keyword.json", {
+    //       headers: {
+    //         Authorization: `KakaoAK ${process.env.KAKAO_KEY}`,
+    //       },
+    //       params: {
+    //         query: query,
+    //         page: 2,
+    //         size: 10,
+    //         category_group_code: "FD6",
+    //         x: center.lng,
+    //         y: center.lat,
+    //         radius: 1500,
+    //       },
+    //     })
+    //     .catch((err) => {
+    //       console.log("에러 발생", err);
+    //     });
+    //     const data = kakaoRespond.data.documents
+    //     // console.log(data)
+    //     res.status(200).json(data)
   } catch (err) {
     // console.log(err);
     res.status(500).send("카카오 api 호출 실패");
