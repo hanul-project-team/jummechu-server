56b4a4efd3a2aa716b735993522c77ac77eee391
STMT <elahep113@naver.com>
2025-05-19 12:50:20 +0900

    chore: 음식점과 카페 정보 요청 1. 카페와 음식점의 카테고리 코드 둘다 필요함. 하지만 카카오는 한가지 요청만 접수 2. 따라서 map으로 요청을 하면서 Promise.all을 통해 병렬 처리를 한뒤 3. 불러온 데이터에 배열에 섞여있을 경우 풀어주기 위해 flatMap을 통해 처리함 4. 결과 : 음식점이든 카페든 둘다 검색어를 통한 검색이 가능해짐

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -37,30 +37,39 @@ router.post("/nearplace", async (req, res) => {
 router.post("/search", async (req, res) => {
   const query = req.body.place;
   const center = req.body.center;
+  const categoryCode = ['FD6', 'CE7']
   try {
     console.log("검색어:", query);
     console.log("좌표", center);
-    //   const kakaoRespond = await axios
-    //     .get("https://dapi.kakao.com/v2/local/search/keyword.json", {
-    //       headers: {
-    //         Authorization: `KakaoAK ${process.env.KAKAO_KEY}`,
-    //       },
-    //       params: {
-    //         query: query,
-    //         page: 2,
-    //         size: 10,
-    //         category_group_code: "FD6",
-    //         x: center.lng,
-    //         y: center.lat,
-    //         radius: 1500,
-    //       },
-    //     })
-    //     .catch((err) => {
-    //       console.log("에러 발생", err);
-    //     });
-    //     const data = kakaoRespond.data.documents
-    //     // console.log(data)
-    //     res.status(200).json(data)
+    
+    const requests = categoryCode.map(code => 
+      axios
+      .get("https://dapi.kakao.com/v2/local/search/keyword.json", {
+        headers: {
+          Authorization: `KakaoAK ${process.env.KAKAO_KEY}`,
+        },
+        params: {
+          query: query,
+          page: 2,
+          size: 10,
+          category_group_code: code,
+          x: center.lng,
+          y: center.lat,
+          radius: 1500,
+        },
+      })
+      .catch((err) => {
+        console.log("에러 발생", err);
+      })
+    )
+    const kakaoResponse = await Promise.all(requests)
+    const kakaoData = kakaoResponse.flatMap(res => res.data.documents);
+    // console.log("가게정보", kakaoData);
+    const data = kakaoData.map(kd => 
+      kd.category_name+'/'+kd.address_name+'/'+kd.place_name
+    )
+    console.log('간단정보:',data)
+    res.status(200).json(kakaoData);
   } catch (err) {
     // console.log(err);
     res.status(500).send("카카오 api 호출 실패");
