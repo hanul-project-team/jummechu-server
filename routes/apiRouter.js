b2668cce6e03ec1f43f38ca45b8869b334315f89
302-09\HANUL <sehwan4104@gmail.com>
2025-05-21 09:37:15 +0900

    1.카카오 api 오류 주석 처리
    2.Ai 이미지 추출 프롬프트 수정

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -7,32 +7,32 @@ router.post("/nearplace", async (req, res) => {
   const center = req.body.location;
   // console.log(center);
   // res.json(center)
-  try {
-    const kakaoRespond = await axios
-      .get("https://dapi.kakao.com/v2/local/search/keyword.json", {
-        headers: {
-          Authorization: `KakaoAK ${process.env.KAKAO_KEY}`,
-        },
-        params: {
-          query: "맛집",
-          page: 2,
-          size: 10,
-          category_group_code: "FD6",
-          x: center.lng,
-          y: center.lat,
-          radius: 1500,
-        },
-      })
-      .catch((err) => {
-        console.log("에러 발생", err);
-      });
-    const data = kakaoRespond.data.documents;
-    // console.log(data)
-    res.status(200).json(data);
-  } catch (err) {
-    // console.log(err);
-    res.status(500).send("카카오 api 호출 실패");
-  }
+  // try {
+  //   const kakaoRespond = await axios
+  //     .get("https://dapi.kakao.com/v2/local/search/keyword.json", {
+  //       headers: {
+  //         Authorization: `KakaoAK ${process.env.KAKAO_KEY}`,
+  //       },
+  //       params: {
+  //         query: "맛집",
+  //         page: 2,
+  //         size: 10,
+  //         category_group_code: "FD6",
+  //         x: center.lng,
+  //         y: center.lat,
+  //         radius: 1500,
+  //       },
+  //     })
+  //     .catch((err) => {
+  //       console.log("에러 발생", err);
+  //     });
+  //   const data = kakaoRespond.data.documents;
+  //   // console.log(data)
+  //   res.status(200).json(data);
+  // } catch (err) {
+  //   // console.log(err);
+  //   res.status(500).send("카카오 api 호출 실패");
+  // }
 });
 router.post("/search", async (req, res) => {
   const query = req.body.place;
