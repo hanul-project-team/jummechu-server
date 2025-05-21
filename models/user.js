3d5e1216143f5f0e40f090e72ffd055a00c45a5e
302-09\HANUL <sehwan4104@gmail.com>
2025-05-21 17:59:13 +0900

    user.js 기능 추가
    1.닉네임 랜덤 생성 함수 추가
    RandomNickname

--- models/user.js
+++ models/user.js
@@ -1,6 +1,20 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
+ const RandomNickname = () => {
+        const first = ["치킨을", "피자를", "짬뽕을", "족발을", "국밥을", "돈까스", "디저트를"]
+        const second = ["먹고싶은", "먹고있는", "좋아하는"]
+        const third = ["배고픈", "행복한", "심심한", "졸린", "굶주린", "화난", "야근하는", "잔업중인"]
+        const last = ["강아지", "고양이", "호랑이", "상어", "비버", "족제비", "곰", "직장인"]
+
+        const firstIndex = Math.floor(Math.random() * first.length);
+        const secondIndex = Math.floor(Math.random() * second.length);
+        const thirdIndex = Math.floor(Math.random() * third.length);
+        const lastIndex = Math.floor(Math.random() * last.length);
+
+        return `${first[firstIndex]} ${second[secondIndex]} ${third[thirdIndex]} ${last[lastIndex]}`;  
+    };
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -14,6 +28,12 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+  },
+  nickname: {
+    type: String,
+    required: true,
+    unique: true,
+    default: RandomNickname,
   },
   phone: {
     type: String,
