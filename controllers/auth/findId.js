a0f8dfa24307832f29a2f42cacb2facbd386be44
MunSeungJun <sbs2469@naver.com>
2025-05-22 17:03:03 +0900

    feat: 아이디 찾기 컨트롤러 생성 및 라우팅 설정

---
+++ controllers/auth/findId.js
@@ -0,0 +1,24 @@
+import User from "../../models/user.js";
+
+export const findId = async (req, res) => {
+  const { name, phone, code } = req.body;
+  try {
+    const user = await User.findOne({ name, phone });
+    if (!user) {
+      return res.status(200).json({
+        user: null,
+        message: "존재하지 않는 사용자입니다.",
+      });
+    }
+    res.status(200).json({
+      user: {
+        name: user.name,
+        email: user.email,
+        createdAt: user.createdAt,
+      },
+      message: "입력하신 정보와 일치하는 아이디를 찾았습니다.",
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
