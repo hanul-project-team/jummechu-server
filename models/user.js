9e7efb43320152c2a568893d4a9b88407a610105
302-09\HANUL <sehwan4104@gmail.com>
2025-05-23 17:44:02 +0900

    랜덤 닉네임 생성 수정
    함수를 user.js -> regist.js 로 이동함

--- models/user.js
+++ models/user.js
@@ -1,19 +1,7 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
- const RandomNickname = () => {
-        const first = ["치킨을", "피자를", "짬뽕을", "족발을", "국밥을", "돈까스", "디저트를"]
-        const second = ["먹고싶은", "먹고있는", "좋아하는"]
-        const third = ["배고픈", "행복한", "심심한", "졸린", "굶주린", "화난", "야근하는", "잔업중인"]
-        const last = ["강아지", "고양이", "호랑이", "상어", "비버", "족제비", "곰", "직장인"]
 
-        const firstIndex = Math.floor(Math.random() * first.length);
-        const secondIndex = Math.floor(Math.random() * second.length);
-        const thirdIndex = Math.floor(Math.random() * third.length);
-        const lastIndex = Math.floor(Math.random() * last.length);
-
-        return `${first[firstIndex]} ${second[secondIndex]} ${third[thirdIndex]} ${last[lastIndex]}`;  
-    };
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -31,9 +19,7 @@ const userSchema = new mongoose.Schema({
   },
   nickname: {
     type: String,
-    required: true,
     unique: true,
-    default: RandomNickname,
   },
   phone: {
     type: String,
