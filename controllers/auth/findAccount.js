1a59642f1e1761cd3ba554efb002fa34780ee4bd
seungjun <sbs2469@naver.com>
2025-05-28 00:37:35 +0900

    feat: 계정 찾기 컨트롤러 sms인증 코드 검증 로직 추가

--- controllers/auth/findAccount.js
+++ controllers/auth/findAccount.js
@@ -1,16 +1,36 @@
 import User from "../../models/user.js";
+import jwt from 'jsonwebtoken'
 
 export const findAccount = async (req, res) => {
   const { name, phone, code } = req.body;
+  if (!code) {
+    return res.status(400).json({
+      message: "인증 코드가 필요합니다.",
+    });
+  }
+  const smsVerifyToken = req.cookies.sms_verify_token;
+  if (!smsVerifyToken) {
+    return res.status(400).json({
+      message: "인증이 필요합니다.",
+    });
+  }
   try {
+    const decoded = jwt.verify(smsVerifyToken, process.env.SMS_VERIFY_KEY);
+    const savedCode = decoded.code;
+
+    if (code !== savedCode) {
+      return res.status(400).json({
+        message: "인증 코드가 일치하지 않습니다.",
+      });
+    }
     const user = await User.findOne({ name, phone });
     if (!user) {
       return res.status(200).json({
         userFound: false,
         user: {
-          name: '',
-          email: '',
-          createdAt: '',
+          name: "",
+          email: "",
+          createdAt: "",
         },
         message: "존재하지 않는 사용자입니다.",
       });
