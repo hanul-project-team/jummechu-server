5abd80a35241d48c2e201b25034034737a90bbbd
MunSeungJun <sbs2469@naver.com>
2025-05-27 16:54:26 +0900

    feat: 라우팅 설정 추가

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { regist } from "../controllers/auth/regist.js";
 import { login } from "../controllers/auth/login.js";
-import { check } from "../controllers/auth/check.js";
-import { logout } from "../controllers/auth/logout.js";
 import { findAccount } from "../controllers/auth/findAccount.js";
 import { target } from "../controllers/auth/target.js";
+import { sendLink } from "../controllers/auth/sendLink.js";
+import { verifyResetToken } from "../controllers/auth/verifyResetToken.js";
+import { resetPassword } from "../controllers/auth/resetPassword.js";
+import { logout } from "../controllers/auth/logout.js";
+import { restoreLogin } from "../controllers/auth/restoreLogin.js";
 
 const router = express.Router();
 
@@ -12,7 +15,10 @@ router.post("/regist", regist);
 router.post("/login", login);
 router.post("/find_account", findAccount);
 router.post("/target", target);
-router.get("/check", check);
+router.post("/send_link", sendLink);
+router.post("/verify_reset_token", verifyResetToken);
+router.post("/reset_password", resetPassword);
 router.get("/logout", logout);
+router.get("/restore_login", restoreLogin);
 
 export default router;
