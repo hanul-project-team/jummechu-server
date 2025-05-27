1a5f1a4c6cb10a6e15a7a90059356ece77c0f7f1
STMT <elahep113@naver.com>
2025-05-27 15:19:57 +0900

    feat: 리뷰 등록후 새로 불러와서 반영하는 로직 추가

--- controllers/reviews/regist.js
+++ controllers/reviews/regist.js
@@ -2,18 +2,25 @@ import Review from "../../models/review.js";
 
 const regist = async (req, res) => {
   const data = req.body;
-//   console.log(data)
+  //   console.log(data)
   const newReview = new Review({
     user: data.user,
     comment: data.comment,
     store: data.store,
-    rating: data.rating
-  })
-//   console.log(newReview)
+    rating: data.rating,
+  });
+  //   console.log(newReview)
   await newReview.save();
+  const returnReviews = await Review.find({ store: data.store })
+    .populate("store", "name address")
+    .populate("user", "name");
+  if (returnReviews.length < 1) {
+    return res.status(204).json(returnReviews);
+  }
   res.status(201).json({
-    msg: '성공적으로 리뷰를 작성하였습니다.'
-  })
+    msg: "성공적으로 리뷰를 작성하였습니다.",
+    data: returnReviews
+  });
 };
 
 export default regist;
