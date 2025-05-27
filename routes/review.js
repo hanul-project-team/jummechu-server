5fa59bc5ba4b39f3e3495e106462b8556069faf9
STMT <elahep113@naver.com>
2025-05-27 15:20:21 +0900

    chore: 리뷰 전체 요청과 id 요청 분리하고 경로 추가

--- routes/review.js
+++ routes/review.js
@@ -1,9 +1,11 @@
 import express from 'express'
 const router = express.Router();
 import regist from '../controllers/reviews/regist.js'
-import readAll from '../controllers/reviews/readAll.js'
+import readReviews from '../controllers/reviews/readReviews.js'
+import readAll from '../controllers/reviews/readAllReviews.js'
 
-router.get('/read/:id', readAll)
+router.get('/read/:id', readReviews)
+router.get('/readall', readAll)
 router.post('/regist', regist)
 
 export default router
