a264bd5cd52f108b6b56593d4df50bc55dff3733
STMT <elahep113@naver.com>
2025-05-27 09:17:12 +0900

    feat: 검색 장소 주변 정보 불러오기 1. 고민 해봐야함 api요청이 많이 들어감 2. 요청 주소 구체화

--- controllers/api/callKakaoApi.js
+++ controllers/api/callKakaoApi.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import generateKeyAndDesc from "../openai_keyword/callOpenai.js";
 import "dotenv/config";
 
-router.post('/nearplace', async (req, res) => {
+router.post('/user/nearplace', async (req, res) => {
   const center = req.body.location;
   // console.log(center);
   try {
@@ -81,6 +81,40 @@ router.post('/search', async (req, res) => {
       })
     )
     // console.log('간단정보:',data)
+    res.status(200).json(data);
+  } catch (err) {
+    // console.log(err);
+    res.status(500).send("카카오 api 호출 실패");
+  }
+})
+router.post('/search/:id', async (req, res) => {
+  const location = req.body.headers
+  const lat = location.lat
+  const lng = location.lng
+  // console.log(location)
+  // console.log(lat)
+  // console.log(lng)
+  try {
+    const kakaoRespond = await axios
+      .get("https://dapi.kakao.com/v2/local/search/keyword.json", {
+        headers: {
+          Authorization: `KakaoAK ${process.env.KAKAO_KEY}`,
+        },
+        params: {
+          query: "맛집",
+          page: 2,
+          size: 15,
+          category_group_code: "FD6",
+          x: lng,
+          y: lat,
+          radius: 3000,
+        },
+      })
+      .catch((err) => {
+        console.log("에러 발생", err);
+      });
+    const data = kakaoRespond.data.documents;
+    // console.log(data)
     res.status(200).json(data);
   } catch (err) {
     // console.log(err);
