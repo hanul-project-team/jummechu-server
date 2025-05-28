d24c0d27afd15301d23e46a83c942a9f30e26d1d
STMT <elahep113@naver.com>
2025-05-28 16:05:13 +0900

    chore: 라우터 파일 명칭, 위치, 상대주소 변경

--- routes/review.js
+++ routes/review.js
@@ -1,8 +1,8 @@
 import express from 'express'
 const router = express.Router();
-import regist from '../controllers/reviews/regist.js'
-import readReviews from '../controllers/reviews/readReviews.js'
-import readAll from '../controllers/reviews/readAllReviews.js'
+import regist from '../controllers/store/registReview.js'
+import readReviews from '../controllers/store/readReviews.js'
+import readAll from '../controllers/store/readAllReviews.js'
 
 router.get('/read/:id', readReviews)
 router.post('/readall', readAll)
--- routes/store.js
+++ routes/store.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import save from "../controllers/store/save.js";
+import save from "../controllers/store/saveStore.js";
 
 router.post("/save", save);
 
