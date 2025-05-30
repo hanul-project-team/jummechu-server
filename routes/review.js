ccab7dea633be9b19c58f4dced345742fc464fe7
STMT <elahep113@naver.com>
2025-05-30 15:54:55 +0900

    feat: 가게 상세 페이지에서 리뷰 삭제 기능

--- routes/review.js
+++ routes/review.js
@@ -3,9 +3,11 @@ const router = express.Router();
 import regist from '../controllers/store/registReview.js'
 import readReviews from '../controllers/store/readReviews.js'
 import readAll from '../controllers/store/readAllReviews.js'
+import deleteReview from '../controllers/store/deleteReview.js'
 
 router.get('/read/:id', readReviews)
 router.post('/readall', readAll)
 router.post('/regist', regist)
+router.delete('/delete/:id', deleteReview)
 
 export default router
