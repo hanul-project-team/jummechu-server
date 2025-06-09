107e8a35893786cd3251d97c763b71a8e1d3bc35
STMT <elahep113@naver.com>
2025-06-09 16:24:45 +0900

    feat: 리뷰 수정 라우터 연결

--- routes/review.js
+++ routes/review.js
@@ -5,11 +5,13 @@ import readStoreReviews from '../controllers/store/readStoreReviews.js'
 import readUserReviews from '../controllers/user/readUserReviews.js'
 import readAll from '../controllers/store/readAllReviews.js'
 import deleteReview from '../controllers/store/deleteReview.js'
+import modifyReview from '../controllers/store/modifyReview.js'
 
 router.get('/read/store/:id', readStoreReviews)
 router.get('/read/user/:id', readUserReviews)
 router.post('/readall', readAll)
 router.post('/regist', regist)
 router.delete('/delete/:id', deleteReview)
+router.put('/:id', modifyReview)
 
 export default router
