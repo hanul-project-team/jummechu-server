9bf7d23b911a8543802bb54559518a7814349ec2
MunSeungJun <sbs2469@naver.com>
2025-06-09 18:06:32 +0900

    feat: static 설정 추가

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
+import path from 'path'
+import { fileURLToPath } from 'url'
 import 'dotenv/config'
 import connect from './config/db.js'
 import apiRouter from './routes/api.js'
@@ -9,6 +11,8 @@ import openaiRoutes from './routes/openai.js'
 import dalleRoutes from './routes/dalle.js'
 
 const app = express()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 app.use(cors({
     origin: 'http://localhost:5173',
@@ -16,6 +20,7 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use('/static', express.static(path.join( __dirname, 'static')))
 app.use(cookieParser())
 connect()
 app.use('/api', apiRouter)
