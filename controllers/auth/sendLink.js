68938e93a5fe86cba53fc7b462429100d8c594d6
MunSeungJun <sbs2469@naver.com>
2025-06-10 18:00:38 +0900

    feat: nhn cloud 이메일 발송 api 추가

--- controllers/auth/sendLink.js
+++ controllers/auth/sendLink.js
@@ -1,5 +1,6 @@
 import User from "../../models/user.js";
 import jwt from "jsonwebtoken";
+import axios from "axios";
 
 export const sendLink = async (req, res) => {
   const { email } = req.body;
@@ -11,11 +12,33 @@ export const sendLink = async (req, res) => {
     const resetToken = jwt.sign({ email }, process.env.RESET_SECRET_KEY, {
       expiresIn: "10m",
     });
-    const resetLink = `http:localhost:5173/find_account/reset?token=${resetToken}`;
-    console.log(resetLink)
+    const resetLink = `http://localhost:5173/find_account/reset?token=${resetToken}`;
+
+    await axios.post(
+      `https://email.api.nhncloudservice.com/email/v2.1/appKeys/${process.env.NHN_APP_KEY}/sender/mail`,
+      {
+        templateId: process.env.NHN_TEMPLATE_ID,
+        templateParameter: {
+          username: user.name,
+          link: resetLink,
+        },
+        receiverList: [
+          {
+            receiveMailAddr: email,
+            receiveName: user.name,
+            receiveType: "MRT0",
+          },
+        ],
+      },
+      {
+        headers: {
+          "Content-Type": "application/json;charset=UTF-8",
+          "X-Secret-Key": process.env.NHN_SECRET_KEY,
+        },
+      }
+    );
     res.status(200).json({
       message: "비밀번호 재설정 링크가 이메일로 전송되었습니다.",
-      resetLink: resetLink
     });
   } catch (e) {
     console.log(e);
