cb73d09b03e1254476b20da30f3906509810c057
STMT <elahep113@naver.com>
2025-06-12 13:07:24 +0900

    feat: 가게정보 누락시 재요청을 위한 코드 추가

---
+++ controllers/store/getStoreInfo.js
@@ -0,0 +1,22 @@
+import Store from "../../models/store.js";
+
+const getStoreInfo = async (req, res) => {
+  const storeId = req.params.id;
+  if (!storeId) {
+    return res.status(400).json({
+      msg: "잘못된 접근입니다.",
+    });
+  }
+  const foundStore = await Store.findOne({ _id: storeId });
+  try {
+    if (foundStore) {
+      return res.status(200).json(foundStore);
+    }
+  } catch (err) {
+    return res.status(500).json({
+      msg: "가게정보 없음.",
+    });
+  }
+};
+
+export default getStoreInfo;
