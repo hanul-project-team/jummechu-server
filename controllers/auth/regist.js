42c4febfd010dfe28c8eb79c34ab00655876f786
MunSeungJun <sbs2469@naver.com>
2025-06-16 17:57:21 +0900

    feat: regist controller  요청 body 수정

--- controllers/auth/regist.js
+++ controllers/auth/regist.js
@@ -1,27 +1,28 @@
 import User from "../../models/user.js";
 
-export const regist = async(req, res) => {
-    const { email, password, name, phone, role, termsAgreement } = req.body
-    try {
-        const exisUser = await User.findOne({email})
-        if(exisUser) {
-            return res.status(400).json({
-                message:'사용 중인 이메일 입니다'
-            })
-        }
-        const newUser = new User({
-            email,
-            password,
-            phone,
-            name,
-            role,
-            agreement: termsAgreement
-        })
-        await newUser.save()
-        res.status(200).json({
-            message:'회원가입 성공'
-        })
-    } catch(e) {
-        console.log(e)
+export const regist = async (req, res) => {
+  const { email, password, name, phone, role, service, privacy, business } =
+    req.body;
+  try {
+    const exisUser = await User.findOne({ email });
+    if (exisUser) {
+      return res.status(400).json({
+        message: "사용 중인 이메일 입니다",
+      });
     }
-}
+    const newUser = new User({
+      email,
+      password,
+      phone,
+      name,
+      role,
+      agreement: { service, privacy, business },
+    });
+    await newUser.save();
+    res.status(200).json({
+      message: "회원가입 성공",
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
