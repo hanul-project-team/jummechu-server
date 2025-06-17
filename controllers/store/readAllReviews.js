73a9a099976fae802cffd86707494021543cfac8
STMT <elahep113@naver.com>
2025-06-17 10:15:38 +0900

    refactor: home화면 리뷰 불러오는 과정 리팩토링

--- controllers/store/readAllReviews.js
+++ controllers/store/readAllReviews.js
@@ -1,35 +1,23 @@
 import Review from "../../models/review.js";
-import Store from '../../models/store.js'
-import generateKeyAndDesc from '../openai_keyword/callOpenai.js'
 
 const readReviews = async (req, res) => {
-  const places = req.body.places
-  // console.log(places)
-  const placesCondition = places.map((pl, i) => ({
-    name: pl.name,
-    address: pl.address
-  }))
+  const requestedPlaces = req.body.places;
+  const places = requestedPlaces.flat();
 
   try {
-    const allStores = await Store.find({$or:placesCondition})
-    if(!allStores || allStores.length < 1) {
-      return res.status(204).json({
-        msg: '일치하는 가게 정보가 존재하지 않습니다.'
-      })
-    }
-
-    const storeIds = allStores.map(as => as._id)
-    if(!storeIds || storeIds.length < 1) {
+    const storeIds = places.map((place) => place._id);
+    if (!storeIds || storeIds.length < 1) {
       return res.status(500).json({
-        msg: '아이디를 불러올수 없습니다.'
-      })
+        msg: "아이디를 불러올수 없습니다.",
+      });
     }
 
-    const allReviews = await Review.find({store: {$in:storeIds}}).populate('store', 'name address')
-    
-    res.status(200).json({
-      data: {allReviews, allStores}
-    });
+    const allReviews = await Review.find({ store: { $in: storeIds } }).populate(
+      "store",
+      "name address"
+    );
+
+    res.status(200).json(allReviews);
   } catch (err) {
     console.log(err);
   }
