4d58d1996f06e93c412d396f1c2b7a2e005fe95d
STMT <elahep113@naver.com>
2025-06-18 12:08:55 +0900

    chore: storeId를 읽지 못하는 에러를 발견 (시점과 원인 모름) 읽지 못할시에 status 400을 반환하도록 설정

--- controllers/store/readStoreReviews.js
+++ controllers/store/readStoreReviews.js
@@ -2,15 +2,20 @@ import Review from "../../models/review.js";
 
 const readStoreReviews = async (req, res) => {
   const storeId = req.params.id;
-    // console.log(storeId);
 
+  if (storeId === undefined || storeId === null) {
+    console.log("storeId를 읽을수 없습니다..");
+    return res.status(400).json({
+      msg: "잘못된 요청입니다.",
+    });
+  }
   try {
     const foundReviews = await Review.find({ store: storeId })
       .populate("store", "name address")
       .populate("user", "name");
-      if(foundReviews.length < 1) {
-        return res.status(204).json(foundReviews)
-      }
+    if (foundReviews.length < 1) {
+      return res.status(204).json(foundReviews);
+    }
     res.status(200).json(foundReviews);
   } catch (err) {
     console.log(err);
